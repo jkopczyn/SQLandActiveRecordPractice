ac3b0cbc2da51a9e9f49d6f1b0f06a794b8a63d1
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-03-03 13:18:38 -0800

    Added some seeding data

--- import_db.sql
+++ import_db.sql
@@ -15,26 +15,49 @@ CREATE TABLE questions (
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
-  FOREIGN KEY (question_id) REFERENCES questions(id),
   user_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
   parent_question INTEGER NOT NULL,
-  FOREIGN KEY (parent_question) REFERENCES questions(id),
   parent_reply INTEGER,
-  FOREIGN KEY (parent_reply) REFERENCES replies(id),
-  body TEXT NOT NULL
+  body TEXT NOT NULL,
+  FOREIGN KEY (parent_question) REFERENCES questions(id),
+  FOREIGN KEY (parent_reply) REFERENCES replies(id)
 );
 
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
-  FOREIGN KEY (question_id) REFERENCES questions(id),
   user_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Enoch', 'Root'), ('Hoid', 'Wit'), ('Jenny', 'Everywhere'), ('Paul', 'Chandler')
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('I have never begged you before', 'I do so now.', 2),
+  ('Alchemy', 'Just because you only see me around alchemists you assume 
+    I am an alchemist. Should I assume the same of you?', 1),
+  ('Oath', 'This is my power, this is my fight, shine through the void with orange light', 4)
+
+INSERT INTO
+  replies (parent_question, parent_reply, body, author_id)
+VALUES
+  (1, NULL, 'Lorem ipsum dolor sit amet. Nil consectur, nil vescit.', 3),
+  (1, 1, 'O sibili si ergo, fortibus es inero. O nobili desar trux.', 2),
+  (1, 2, 'Si quad sinem? Causen dux.', 3),
+  (2, NULL, 'Pellentesque vitae augue mauris. In vel mattis justo. Nulla pulvinar dolor in massa suscipit, at sodales orci interdum. Quisque et tempor purus, vitae pretium libero. Duis faucibus velit in mattis pellentesque.', 4)
+
+INSERT INTO
+  
 
